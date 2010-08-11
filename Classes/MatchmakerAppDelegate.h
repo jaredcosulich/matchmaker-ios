c0d1edff0ca012d050ff1f1b908731e4c6598cf6
Erik Hanson <erik@eahanson.com>
2010-08-11 16:36:03 -0700

    first iOS proj

---
+++ Classes/MatchmakerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MatchmakerAppDelegate.h
+//  Matchmaker
+//
+//  Created by Erik Hanson on 8/11/10.
+//  Copyright 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchmakerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
